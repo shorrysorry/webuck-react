7fc7d9f3987717e25f1ca9cef2dc14882968c49a
김수빈 <ksb334ksb@gmail.com>
2022-03-04 17:01:25 +0900

    Mission 2 done & Password field is not contained in a form Error occur

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,18 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Login.scss';
 
 function Login() {
+  const [id, setId] = useState('');
+  const [pw, setPw] = useState('');
+
+  const handleIdInput = e => {
+    if (e.target.classList.contains('email')) {
+      setId(e.target.value);
+    } else if (e.target.classList.contains('pw')) {
+      setPw(e.target.value);
+    }
+  };
+
   return (
     <div className="loginSubin">
       <section id="loginSection">
         <h2 id="title">WeBucks</h2>
 
-        <div className="inputSection" action="">
+        <div className="inputSection">
           <div className="idWrapper">
             <input
               className="loginInput email"
               type="text"
               placeholder="전화번호,사용자 이름 또는 이메일"
+              onChange={handleIdInput}
+              value={id}
             />
           </div>
           <div className="pwWrapper">
@@ -20,6 +33,8 @@ function Login() {
               className="loginInput pw"
               type="password"
               placeholder="비밀번호"
+              onChange={handleIdInput}
+              value={pw}
             />
             <i className="fa-solid fa-eye-slash eye" />
           </div>
